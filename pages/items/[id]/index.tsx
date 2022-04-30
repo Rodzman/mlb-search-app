4deb7d8804bdbcaac5c0a6a3e3aa77d9c6f0f6e2
Rodrigo Pascoalino <designrsp@gmail.com>
2022-04-30 14:29:09 -0300

    ML-6 - Configuracao inicial do projeto

---
+++ pages/items/[id]/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Item: NextPage = () => {
+    return (<div>Item Page</div>);
+}
+
+export default Item;
---
+++ pages/items/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Items: NextPage = () => {
+    return (<div>Items</div>);
+}
+
+export default Items;

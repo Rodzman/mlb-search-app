b05ea3dd6f822bbd2eb2c5dc881157540669d11a
Rodrigo Pascoalino <designrsp@gmail.com>
2022-05-01 15:40:05 -0300

    ML-12 - Configuracao da estrutura de testes da aplicacao

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.(j|t)sx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json'
+    }
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  }
+};
